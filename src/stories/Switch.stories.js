90ac013e5465621611c3cb95d9f95b6ad0207258
Fredrik Jungstedt <fredrik@properdesign.se>
2024-09-30 16:24:24 +0200

    Colored switches

--- src/stories/Switch.stories.js
+++ src/stories/Switch.stories.js
@@ -1,17 +1,17 @@
-import Switch from '../components/core/Switch.vue';
+import Switch from "../components/core/Switch.vue";
 
 export default {
-  title: 'Core/Switch',
+  title: "Core/Switch",
   component: Switch,
   argTypes: {
-    'onUpdate:modelValue': { action: 'clicked' },
+    "onUpdate:modelValue": { action: "clicked" },
     modelValue: {
-      type: 'boolean'
-    }
+      type: "boolean",
+    },
   },
   args: {
     modelValue: true,
-  }
+  },
 };
 
 const Template = (args) => ({
@@ -24,13 +24,31 @@ const Template = (args) => ({
 
 export const Default = Template.bind({});
 Default.args = {
-  label: 'Switch this!'
+  label: "Switch this!",
+};
+
+export const Success = Template.bind({});
+Success.args = {
+  label: "You should switch this!",
+  type: "success",
+};
+
+export const Warning = Template.bind({});
+Warning.args = {
+  label: "Are you sure you want to switch this?",
+  type: "warning",
+};
+
+export const Danger = Template.bind({});
+Danger.args = {
+  label: "It's dangerous to switch this!",
+  type: "danger",
 };
 
 export const Complex = Template.bind({});
 Complex.args = {
-  label: 'Switch this!',
+  label: "Switch this!",
   indeterminate: true,
-  'true-value': {foo: 'bar'},
-  'false-value': {foo: 'baz'},
+  "true-value": { foo: "bar" },
+  "false-value": { foo: "baz" },
 };
