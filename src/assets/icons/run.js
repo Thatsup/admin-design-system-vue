164eb213517fc5616a504597f2298554976084a5
Fredrik Jungstedt <fredrik@properdesign.se>
2024-10-01 14:40:51 +0200

    New icons

--- src/assets/icons/run.js
+++ src/assets/icons/run.js
@@ -2,21 +2,23 @@ const fs = require("fs"),
   files = fs.readdirSync(__dirname + "/svg/");
 
 var stream = fs.createWriteStream(__dirname + "/icons.js");
-stream.once("open", function() {
+stream.once("open", function () {
   let aliases = [];
 
   stream.write("export const icons = {\n");
 
-  files.forEach(function(file) {
-    if(extension(file) !== 'svg') {
+  files.forEach(function (file) {
+    if (extension(file) !== "svg") {
       return;
     }
     const contents = fs.readFileSync(__dirname + "/svg/" + file, "utf8");
-    let def = contents.replace(/<\/?svg([^>]*)>/ig, '');
-    def = def.replace(/[\t\n\r]/g,'')
-    def = def.replace(/(fill|stroke)="#([^"]+)"/g,'$1="currentColor"')
-    def = def.replace(/(fill|stroke):\s*#([^;]+);/g,'$1:currentColor;')
-    def = def.replace(/(\s){2,}/g,'$1');
+    let def = contents.replace(/<\/?svg([^>]*)>/gi, "");
+    def = def.replace(/<desc>.*<\/desc>/g, "");
+    def = def.replace(/<title>.*<\/title>/g, "");
+    def = def.replace(/[\t\n\r]/g, "");
+    def = def.replace(/(fill|stroke)="#([^"]+)"/g, '$1="currentColor"');
+    def = def.replace(/(fill|stroke):\s*#([^;]+);/g, "$1:currentColor;");
+    def = def.replace(/(\s){2,}/g, "$1");
 
     let fileName = file.split(".")[0];
 
@@ -25,10 +27,10 @@ stream.once("open", function() {
       fileName = parts[0];
       parts.shift();
 
-      parts.forEach(function(part) {
+      parts.forEach(function (part) {
         aliases.push({
           alias: part,
-          name: fileName
+          name: fileName,
         });
       });
     }
@@ -41,7 +43,7 @@ stream.once("open", function() {
   stream.write("}; \n\n");
   stream.write("export const aliases = {\n");
 
-  aliases.forEach(function(alias) {
+  aliases.forEach(function (alias) {
     stream.write(`"${alias.alias}": "${alias.name}", \n`);
   });
 
@@ -49,6 +51,6 @@ stream.once("open", function() {
   stream.end();
 });
 
-const extension = function(file) {
-  return file.split('.').pop();
+const extension = function (file) {
+  return file.split(".").pop();
 };
