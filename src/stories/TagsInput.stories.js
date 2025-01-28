a9b9c79262ae4a2be5d95a8d97e24f23c314b5f1
robin <hej@robin.se>
2025-01-28 10:30:11 +0000

    Added a way to prevent deletion of a tag inside TagsInput.vue

--- src/stories/TagsInput.stories.js
+++ src/stories/TagsInput.stories.js
@@ -23,15 +23,24 @@ const Template = (args) => ({
   components: { TagsInput },
   setup() {
     const data = ref(args.modelValue);
-    return { args, data };
+
+    const handleDelete = (event) => {
+      // Prevent deletion if needed
+      // if (true) {
+      //   event.preventDefault();
+      // }
+    }
+
+    return { args, data, handleDelete };
   },
-  template: '<TagsInput v-bind="args" v-model="data" /><pre>{{data}}</pre>',
+  template: '<TagsInput v-bind="args" v-model="data" @delete:tag="handleDelete" /><pre>{{data}}</pre>',
 });
 
 export const ArrayInput = Template.bind({});
 ArrayInput.args = {
   modelValue: ['Restaurant', 'Bar', 'Night club', 'Café', 'Barber', 'Tattoo parlour', 'Mall', 'Food market'],
   border: true,
+  canDelete: true,
 }
 
 export const ArrayWithObjects = Template.bind({});
